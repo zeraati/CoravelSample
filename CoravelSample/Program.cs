5737fccd7bbedc2fe5687650231f62947f600737
zeraati <zeraatimail@gmail.com>
2024-08-09 16:35:41 +0330

    coravel

--- CoravelSample/Program.cs
+++ CoravelSample/Program.cs
@@ -1,7 +1,13 @@
+using Coravel;
+using Coravel.Invocable;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScheduler();
+builder.Services.AddTransient<ScheduledTask>();
 
 var app = builder.Build();
 app.UseSwagger();
@@ -9,4 +15,18 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.Services.UseScheduler(scheduler =>
+{ scheduler.Schedule<ScheduledTask>().EverySeconds(2); });
+
 app.Run();
+
+
+public class ScheduledTask : IInvocable
+{
+    public Task Invoke()
+    {
+        Console.WriteLine($"Executing scheduled task at: {DateTime.Now}");
+        return Task.CompletedTask;
+    }
+}
